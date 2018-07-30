04b824bc4e030169014e1304c905b96d5e563317
韩淅 <406633257@qq.com>
2018-07-30 21:26:32 +0800

    03-fullpag方法

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/**
+ * Created by xi on 2018/7/29.
+ */
+$(function () {
+   $('#container').fullpage({
+      sectionsColor:['#fadd67','#84a2d4','#ef674d','#ffeedd','#d04759','#84d9ed','#8ac060','#fff'],
+      verticalCentered: false,
+      navigation: true
+
+   });
+});
